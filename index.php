45e8d9cbcbed5859b2c71c0630a9bbcde571c382
IgorKawczynski <kluseczkibabuni94@gmail.com>
2022-11-10 12:55:01 +0100

    Restructuring the project...

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>Moje Hobby to Piłka Nożna</title>
+  <base href="/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
+  <?php
+
+  $autor = ' Igor Kawczynski<br>';
+  $przedmiot = ' Programowanie Aplikacji WWW<br>';
+  $nr_indeksu = ' 162423<br>';
+  $nrGrupy = ' 2<br>';
+
+  echo $autor, $przedmiot, $nr_indeksu, $nrGrupy;
+  
+  ?>
+
+</body>
+
+</html>
